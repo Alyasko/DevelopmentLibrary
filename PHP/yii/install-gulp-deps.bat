1182d6d74d4c6a55aff4f0f0f5ea67425dac9e8b
Alexander <yasko.alexander@gmail.com>
2017-09-11 10:30:09 +0300

    Updated gulp dependencies installation script.

---
+++ PHP/yii/install-gulp-deps.bat
@@ -0,0 +1,7 @@
+call npm install gulp
+call npm install gulp-sass
+call npm install gulp-typescript typescript
+call npm install --save-dev gulp-livereload
+call npm install gulp-clean-css --save-dev
+call npm install --save-dev gulp-uglify
+call npm install --save-dev gulp-obfuscate
