3e624ff27213b4fa5253f2bcb129501c3accfc86
Alyasko <yasko.alexander@gmail.com>
2017-09-17 22:50:38 +0300

    Added ReflectionUtils class with method for public poperties copying.

---
+++ DotNET/Utils/ReflectionUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookPages.Utils.Reflection
+{
+    public static class ReflectionUtils
+    {
+        public static void ClonePublicProperties<TSource, TDest>(TSource sourceObject, TDest destObject)
+        {
+            PropertyInfo[] sourceProperties = typeof(TSource).GetProperties();
+            PropertyInfo[] destProperties = typeof(TDest).GetProperties();
+
+            foreach (PropertyInfo sourceProperty in sourceProperties)
+            {
+                String sourcePropName = sourceProperty.Name;
+
+                foreach (PropertyInfo destProperty in destProperties)
+                {
+                    String destPropName = destProperty.Name;
+
+                    if (sourcePropName.Equals(destPropName) && sourceProperty.GetMethod.IsPublic && destProperty.GetMethod.IsPublic)
+                    {
+                        destProperty.SetValue(destObject, sourceProperty.GetValue(sourceObject));
+                    }
+                }
+            }
+        }
+    }
+}
