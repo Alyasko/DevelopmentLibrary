f621f0724b247466c54693d496741eee287c7bab
Alyasko <yasko.alexander@gmail.com>
2017-10-04 09:53:52 +0300

    Added Error view model.

---
+++ DotNET/ASP.NET/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookPages.ViewModels
+{
+    public class ErrorViewModel
+    {
+        private const String DefaultErrorTitle = "Error occurred during processing of your request";
+        private const String DefaultErrorDescription = "Details";
+
+        public ErrorViewModel() : this(DefaultErrorTitle, DefaultErrorDescription)
+        {
+            
+        }
+
+        public ErrorViewModel(String errorDescription) : this(DefaultErrorTitle, errorDescription)
+        {
+            ErrorDescription = errorDescription;
+        }
+
+        public ErrorViewModel(String errorTitle, String errorDescription)
+        {
+            ErrorTitle = errorTitle;
+            ErrorDescription = errorDescription;
+        }
+
+        public String ErrorTitle { get; set; }
+        public String ErrorDescription { get; set; }
+    }
+}
